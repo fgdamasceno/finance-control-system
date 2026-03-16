c5391bac3c3ae7380cdaec379d7611b524d53cb5
fgdamasceno <fgdamasceno@gmail.com>
2026-03-16 10:08:24 -0300

    fix: disable datasource autoconfiguration to allow application startup

--- backend/finance-control-api/src/main/java/com/financecontrol/api/FinanceControlApiApplication.java
+++ backend/finance-control-api/src/main/java/com/financecontrol/api/FinanceControlApiApplication.java
@@ -2,9 +2,9 @@ package com.financecontrol.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration; // temp config
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // temp config
 public class FinanceControlApiApplication {
 
     public static void main(String[] args) {
